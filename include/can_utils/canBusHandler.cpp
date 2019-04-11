e352d55e3f0c8a873c60f987abb36bef7bc0d70a
Conhokis <filipe_mendes914@hotmail.com>
2019-04-11 15:40:49 +0100

    Update canBusHandler.cpp

--- include/can_utils/canBusHandler.cpp
+++ include/can_utils/canBusHandler.cpp
@@ -207,14 +207,14 @@ void canBusHandler::writeCanFrame(char* str_frame) {
 
 uint8_t* canBusHandler::readCanMsg() {
 	//8 is number of bytes in CAN data frame
-	static uint8_t final_data[8]
+	static uint8_t final_data[8];
 	struct can_frame buff_frame;
-	/*
-	can_frame buff_frame = readCanFrame();
+	
+	buff_frame = readCanFrame();
 
 	while(checkFrame(buff_frame)) {
 		buff_frame = readCanFrame();
-	}*/
+	}
 
 	return 0;
 }
