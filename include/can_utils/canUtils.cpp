c5306ba7e88620691b6f2f3ed7dc3b249b732350
Conhokis <filipe_mendes914@hotmail.com>
2019-04-11 23:06:14 +0100

    Update canUtils.cpp

--- include/can_utils/canUtils.cpp
+++ include/can_utils/canUtils.cpp
@@ -64,19 +64,19 @@ void canMotorInterface::powerOnMotor() {
 
 	//Check if motor is switched on
 	do {
-		_canBH->writeCanFrame(concDataId("#4061600000000000", 0x600));
+		_canBH->writeCanFrame(concDataId("#4041600000000000", 0x600));
 		read_data = _canBH->readCanMsg();
 	} while(((read_data[4] & 0b00110011) != 51) || ((read_data[5] & 0b00000010) != 2));
 
 	//Strt operating mode
 	_canBH->writeCanFrame(concDataId("#2B4060000F000000", 0x600));
-/*
+
 	//Check if motor is in operating mode
 	do {
-		_canBH->writeCanFrame(concDataId("#4061600000000000", 0x600));
+		_canBH->writeCanFrame(concDataId("#4041600000000000", 0x600));
 		read_data = _canBH->readCanMsg();
-	} while(((read_data[4] & 0b00110111) != 51) || ((read_data[5] & 0b00000010) != 2));
-*/
+	} while(((read_data[4] & 0b00110111) != 55) || ((read_data[5] & 0b00000010) != 2));
+
 	std::cout << "Motor is ready for operation!" << std::endl;
 }
 
