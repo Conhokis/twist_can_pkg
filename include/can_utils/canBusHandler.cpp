7809b00b56654aaccf15d4860029aee597999b9e
Conhokis <filipe_mendes914@hotmail.com>
2019-04-11 17:51:37 +0100

    Update canBusHandler.cpp

--- include/can_utils/canBusHandler.cpp
+++ include/can_utils/canBusHandler.cpp
@@ -47,6 +47,8 @@ canBusHandler::canBusHandler(const char* can_interface, unsigned int *node_id, s
 	for(int iter = 0; iter++; iter < N_EXCLUSIONS)
 		id_excl[iter] = node_id[iter] + 600;
 
+	printf("%d\n", node_id[0]);
+	printf("%d\n", node_id[1]);
 	printf("%d\n", id_excl[0]);
 	printf("%d\n", id_excl[1]);
 	
